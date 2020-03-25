c5ca2866ad417688228980fab5bb785531e7808b
Sweata1501 <62567640+Sweata1501@users.noreply.github.com>
2020-03-25 23:59:46 +0530

    Update modified_draws.R

--- modified_draws.R
+++ modified_draws.R
@@ -1,13 +1,13 @@
 #modified functions to draw samples
-rExp <- function(n, theta) {
+rExp <- function(n, theta=1) {
   rexp(n, rate = 1/theta)
 }
 
-rGamma <- function(n, theta, shape=0.5) {
+rGamma <- function(n, theta=1, shape=0.5) {
   rgamma(n, shape = shape, scale = theta)
 }
 
-rWeibull <- function(n, theta, shape=0.5){
+rWeibull <- function(n, theta=1, shape=0.5){
   rweibull(n, shape = shape, scale = theta)
 }
 
@@ -15,7 +15,7 @@ rNorm <- function(n, theta=1) {
   rnorm(n, mean = 0, sd = theta)
 }
 
-rCauchy <- function(n, theta, shape=0.5){
+rCauchy <- function(n, theta=1, shape=0.5){
   rcauchy(n, location = shape, scale = theta)
 }
 
