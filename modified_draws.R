9d20098bd1266f09e213bce0d036f8896a1e19b5
Sweata1501 <sen.sweata001@gmail.com>
2020-03-25 23:43:13 +0530

    adding slight changes

---
+++ modified_draws.R
@@ -0,0 +1,24 @@
+#modified functions to draw samples
+rExp <- function(n, theta) {
+  rexp(n, rate = 1/theta)
+}
+
+rGamma <- function(n, theta, shape=0.5) {
+  rgamma(n, shape = shape, scale = theta)
+}
+
+rWeibull <- function(n, theta, shape=0.5){
+  rweibull(n, shape = shape, scale = theta)
+}
+
+rNorm <- function(n, theta=1) {
+  rnorm(n, mean = 0, sd = theta)
+}
+
+rCauchy <- function(n, theta, shape=0.5){
+  rcauchy(n, location = shape, scale = theta)
+}
+
+rLogis <- function(n, theta, shape=0.5){
+  rlogis(n, location = shape, scale = theta)
+}
