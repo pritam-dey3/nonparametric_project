356ca9d14ef945e9d2f01ff1cefb906b7122ce40
pritam3339 <pdeypritam@gmail.com>
2020-04-13 21:25:20 +0530

    final few changes 2

--- 5. power_plot.R
+++ 5. power_plot.R
@@ -2,13 +2,14 @@
 
 # After generating the data this function should be compiled
 # as it only reads the generated data and plots them
-plot_power <- function(n, m, stat, distns, title=""){
+plot_power <- function(n, m, stat, distns, title="", theta_range=c(5, 1, 1000), repl=1000){
   require(lattice)
-  data <- matrix(0, nrow=1000, ncol=length(distns))
+  data <- matrix(0, nrow=repl, ncol=length(distns))
   colnames(data) <- distns
   for (distn in distns){
-    data[, distn] <- as.vector(read.csv(sprintf("data/power -- %s_rejection (%d, %d) -- r%s -- 5.00, 1.00, 1000.00 -- repl=1000.csv",
-                                    stat, n, m, distn))[, -1])
+    data[, distn] <- as.vector(read.csv(sprintf("data/power -- %s_rejection (%d, %d) -- r%s -- %.2f, %.2f, %.2f -- repl=%d.csv",
+                                    stat, n, m, distn, 
+                                    theta_range[1], theta_range[2], theta_range[3], repl))[, -1])
   }
   frml = paste(distns, collapse = " + ")
   frml = formula(sprintf("%s ~ theta", frml))
